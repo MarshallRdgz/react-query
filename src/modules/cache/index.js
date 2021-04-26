37ff696379ac8d4800b1e091c1b82e7e5b9ee057
Jose Balderas <jose.balderas@chiper.co>
2021-04-26 12:40:20 -0500

    example

--- src/modules/cache/index.js
+++ src/modules/cache/index.js
@@ -4,12 +4,33 @@ import { useQuery, useQueryCache, QueryCache } from "react-query";
 const Cache = () => {
   //const queryCache = useQueryCache();
   const { data } = useQuery("listRick");
+  console.log("dataUseQuery--->", data);
+  const generateCart = () => {
+    return data.results.map((item, index) => {
+      return (
+        <div style={{ marginBottom: "20px" }}>
+          <img src={item.image} />
+          <div>Name : {item.name}</div>
+          <div>Status : {item.status}</div>
+          <div>Specie : {item.species}</div>
+        </div>
+      );
+    });
+  };
 
   return (
     <>
-      {data && (
-        <div style={{ width: "100vw", height: "100vh", overflow: "auto" }}>
-          {JSON.stringify(data)}
+      {!!data && (
+        <div
+          style={{
+            width: "100vw",
+            height: "100vh",
+            overflow: "auto",
+            padding: "20px",
+            boxSizing: "border-box",
+          }}
+        >
+          {generateCart()}
         </div>
       )}
     </>
